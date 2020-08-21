0f372f6cf64732a838b269f8a074981b179e8ed2
wodeya163 <43662257+wodeya163@users.noreply.github.com>
2020-08-21 10:21:04 +0800

    获取头像

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,17 +1,25 @@
-// pages/home/home.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    day:0
-
+      nickName:'',
+      avatarUrl:''
+    
+    
   },
-  addNum(){
+  getUserInfo:function(res){
+    var nickname = res.detail.userInfo.nickName
+    var avatar = res.detail.userInfo.avatarUrl
     this.setData({
-      day:this.data.day+1
+      nickName:nickname,
+      avatarUrl:avatar
+    
+
     })
+
+    console.log(res)
 
   },
 
@@ -20,56 +28,54 @@ Page({
    */
   onLoad: function (options) {
     
-    
-
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
   }
 })
