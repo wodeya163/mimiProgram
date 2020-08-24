1621afbe0de55d662c8875bcc2191c71384ebca1
wodeya163 <43662257+wodeya163@users.noreply.github.com>
2020-08-24 16:19:14 +0800

    11components

--- pages/compacg/compacg.js
+++ pages/compacg/compacg.js
@@ -5,6 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
+    title:['第一个标题','第二个标题','第三标题'],
+    content:['内容1','内容2','内容3'],
+    students:[{
+      name:'张三',
+      age:35,
+      gender:'男'
+    },
+    {
+      name:'李二婶',
+      age:45,
+      gender:'女'
+    },
+    {
+    name:'王小二',
+    age:25,
+    gender:'男'
+    },]
 
   },
 
